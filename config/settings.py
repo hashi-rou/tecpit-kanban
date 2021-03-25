9fa8fd5e37ff7e697b08790d5f2a76e9a4cfec02
Kotaro <hk19968104@gmail.com>
2021-03-25 19:22:33 +0900

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -74,6 +74,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
